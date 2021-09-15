cdae006deac0db8fe6761cc8db513d0e6cc24ed4
arthurrenan00 <arthur.pereira48@etec.sp.gov.br>
2021-09-15 11:30:33 -0300

    Adição da pasta repositório e da classe de conexão do MySql

---
+++ ProjetoLojaGames/Repositorio/Conexao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace ProjetoLojaGames.Repositorio
+{
+    public class Conexao
+    {
+        MySqlConnection cn = new MySqlConnection("Server=localhost;DataBase=db_lojagames;user=root;pwd=abacate");
+        public static string msg;
+
+        public MySqlConnection ConectarBd()
+        {
+            try
+            {
+                cn.Open();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao conectar" + erro.Message;
+            }
+            return cn;
+        }
+
+        public MySqlConnection DesconectarBd()
+        {
+            try
+            {
+                cn.Close();
+            }
+            catch (Exception erro)
+            {
+                msg = "Ocorreu um erro ao desconectar" + erro.Message;
+            }
+            return cn;
+        }
+    }
+}
