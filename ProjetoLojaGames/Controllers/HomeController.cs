a3452d9d5785cac24f399f3fe31fe349a5359f73
arthurrenan00 <arthur.pereira48@etec.sp.gov.br>
2021-09-16 10:41:27 -0300

    Adicionando os métodos de cadastrar e listar na home controller

--- ProjetoLojaGames/Controllers/HomeController.cs
+++ ProjetoLojaGames/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjetoLojaGames.Models;
+using ProjetoLojaGames.Repositorio;
 
 namespace ProjetoLojaGames.Controllers
 {
@@ -13,18 +15,73 @@ namespace ProjetoLojaGames.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        Acoes ac = new Acoes();
+        //PARTE DO JOGO
 
-            return View();
+        public ActionResult Jogo()
+        {
+            var jogo = new Jogo();
+            return View(jogo);
+        }
+        
+
+        [HttpPost]
+        public ActionResult CadJogo(Jogo jog)
+        {
+            ac.CadastrarJogo(jog);
+            return View(jog);
         }
 
-        public ActionResult Contact()
+        public ActionResult ListarJogo()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var ExibeJogo = new Acoes();
+            var TodosJogos = ExibeJogo.ListarJogo();
+            return View(TodosJogos);
         }
+
+        // PARTE DO CLIENTE
+        public ActionResult Cliente()
+        {
+            var cliente = new Cliente();
+            return View(cliente);
+        }
+
+
+        [HttpPost]
+        public ActionResult CadCli(Cliente cli)
+        {
+            ac.CadastrarCliente(cli);
+            return View(cli);
+        }
+
+        public ActionResult ListarCliente()
+        {
+            var ExibeCli = new Acoes();
+            var TodosCli = ExibeCli.ListarCliente();
+            return View(TodosCli);
+        }
+
+        // PARTE DO FUNCIONÁRIO
+        public ActionResult Funcionario()
+        {
+            var funcio = new Funcionario();
+            return View(funcio);
+        }
+
+
+        [HttpPost]
+        public ActionResult CadFunc(Funcionario func)
+        {
+            ac.CadastrarFunc(func);
+            return View(func);
+        }
+
+        public ActionResult ListarFuncionario()
+        { 
+            var ExibeFunc = new Acoes();
+            var TodosFunc = ExibeFunc.ListarFuncionario();
+            return View(TodosFunc);
+        }
+
     }
 }
